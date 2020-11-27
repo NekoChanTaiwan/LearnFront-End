3cf78425d6ac4fae4666fc39a0fb6a419a50c662
NekoChan <aaron74327@gmail.com>
2020-11-26 20:22:51 -0800

    完成

--- JavaScript/尚硅谷/全選練習/script.js
+++ JavaScript/尚硅谷/全選練習/script.js
@@ -1,46 +1,63 @@
 "use strict";
-/**
- * * click function
- * @param {*} btnIdStr 按鈕 Id 字符串
- * @param {*} func 函數
- */
-const click = (btnIdStr, func) => {
-    const btn = document.getElementById(btnIdStr);
-    btn.onclick = func;
-};
 
 window.onload = () => {
-    // * 全局變量
+    // * 變量
     const items = document.getElementsByName("items");
     const itemsLength = items.length;
+    const checkedAllBox = document.getElementById("checkedAllBox");
 
-    // TODO: 1.#checkedAllBtn 全選 checkbox
+    // * 函數
+    // 獲取 按鈕
+    /**
+     * @param {*} btnIdStr 按鈕 Id 字符串
+     * @param {*} func 函數
+     */
+    const click = (btnIdStr, func) => {
+        const btn = document.getElementById(btnIdStr);
+        btn.onclick = func;
+    };
+
+    // 檢查目前 items
+    const checkItems = () => {
+        checkedAllBox.checked = true;
+        // console.log('checking...')
+        for (let i = 0; i < itemsLength; i++) {
+            if (!items[i].checked) { // 利用循環判斷，如果有一個沒勾
+                checkedAllBox.checked = false;
+                break; // 條件一滿足後就已經出現結果了，不必要繼續循環
+            }
+        }
+    }
+
+    // TODO: 1.#checkedAllBtn 全選
     click("checkedAllBtn", () => {
         for (let i = 0; i < itemsLength; i++) {
             items[i].checked = true;
         }
+        checkedAllBox.checked = true;
     });
-    // TODO: 2.#checkedNoBtn 取消全選
+
+    // TODO: 2.#checkedNoBtn 全不選
     click("checkedNoBtn", () => {
         for (let i = 0; i < itemsLength; i++) {
             items[i].checked = false;
         }
+        checkedAllBox.checked = false;
     });
+
     // TODO: 3.#checkedRevBtn 反選
     click("checkedRevBtn", () => {
         for(let i = 0; i < itemsLength; i++) {
-            if (items[i].checked == true) {
-                items[i].checked = false;
-            } else {
-                items[i].checked = true;
-            }
+            items[i].checked = !items[i].checked;
         }
+        checkItems();
     });
-    // TODO: 4.#sendBtn
+
+    // TODO: 4.#sendBtn 提交
     click("sendBtn", () => {
         let showMessage = "已提交：";
         for (let i = 0; i < itemsLength; i++) {
-            if (items[i].checked == true) {
+            if (items[i].checked) {
                 if (i > 0) {
                     showMessage += `、${items[i].value}`;
                 } else {
@@ -51,6 +68,18 @@ window.onload = () => {
         console.log(showMessage);
         alert(showMessage);
     });
-    // TODO: 5.#checkedAllBox
-    // TODO: 6.items
+
+    // TODO: 5.#checkedAllBox 全選/全不選
+    click("checkedAllBox", () => {
+        for (let i = 0; i < itemsLength; i++) {
+            items[i].checked = checkedAllBox.checked;
+        }
+    });
+
+    // TODO: 6.items 偵測 全選/全不選
+    for (let i = 0; i < itemsLength; i++) {
+        items[i].onclick = () => {
+            checkItems();
+        }
+    }
 };

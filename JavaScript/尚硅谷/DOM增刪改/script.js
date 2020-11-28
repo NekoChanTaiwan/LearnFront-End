48914a945cdda9439eae639e1e4c844c6fadb2db
NekoChan <aaron74327@gmail.com>
2020-11-27 20:14:19 -0800

    Update script.js

--- JavaScript/尚硅谷/DOM增刪改/script.js
+++ JavaScript/尚硅谷/DOM增刪改/script.js
@@ -1,4 +1,4 @@
-function click(idStr, func) {
+let click = (idStr, func) => {
     const btn = document.getElementById(idStr);
     btn.onclick = func;
 }
@@ -6,7 +6,7 @@ function click(idStr, func) {
 window.onload = function() {
     const city = document.querySelector("#city");
 
-    //創建一個"台南"節點,添加到#city下
+    // TODO: 創建一個"台南"節點,添加到#city下
     click("btn01", () => {
         // <li>台南</li>
         let li = document.createElement("li");
@@ -14,9 +14,13 @@ window.onload = function() {
         li.appendChild(text);
         city.appendChild(li);
     });
-    //將"台南"節點插入到#xb前面
-    //使用"台南"節點替換#xb節點
-    //刪除#xb節點
-    //讀取#city內的HTML代碼
-    //設置#xb內的HTML代碼
+    // TODO: 將"台南"節點插入到#xb前面
+    click("btn02", () => {
+        
+    });
+
+    // TODO: 使用"台南"節點替換#xb節點
+    // TODO: 刪除#xb節點
+    // TODO: 讀取#city內的HTML代碼
+    // TODO: 設置#xb內的HTML代碼
 };
